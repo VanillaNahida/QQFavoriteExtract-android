0a2c42b1758cf7e4e6eee03bf85f81fbef270811
氢氧化喵_NyaOH <12101138+nyaoh@user.noreply.gitee.com>
2024-12-06 14:16:51 +0800

    完成了授予所有文件访问的请求

--- app/src/main/java/com/nahida/qqfavoriteextract/MainActivity.java
+++ app/src/main/java/com/nahida/qqfavoriteextract/MainActivity.java
@@ -1,10 +1,12 @@
 package com.nahida.qqfavoriteextract;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,136 +17,124 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import android.content.SharedPreferences;
 import android.content.Intent;
 import android.os.Environment;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
     // 定义权限请求码
-    private static final int REQUEST_CODE_PERMISSIONS = 1001;
-
-    // 定义SharedPreferences文件名和键名
-    private static final String PREFS_NAME = "AppPrefs";
-    private static final String KEY_FIRST_RUN = "isFirstRun";
-
-    @RequiresApi(api = Build.VERSION_CODES.R)
+    private static final int REQUEST_CODE = 1024;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.button);
-        TextView status = findViewById(R.id.status);
-
-        // 获取SharedPreferences对象
-        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-
-        // 检查是否为首次运行
-        boolean isFirstRun = prefs.getBoolean(KEY_FIRST_RUN, true);
-
-        if (!Environment.isExternalStorageManager()){
-            // 更新首次运行状态
-            prefs.edit().putBoolean(KEY_FIRST_RUN, false).apply();
-        }
-
-        if (isFirstRun) {
-            // 检查权限
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.MANAGE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                // 创建AlertDialog.Builder对象
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("权限请求")
-                        .setMessage("欢迎使用本软件\n本软件需要访问您的存储权限来提取并保存QQ的收藏表情包\n请点击下方“授予存储权限”来授权\n若取消授权，则软件将会退出\n（本窗口只会在未授权的情况下才会弹出）")
-                        .setPositiveButton("授予存储权限", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // 处理确定按钮点击事件
-                                Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
-                                intent.setData(Uri.parse("package:" + getPackageName()));
-                                startActivity(intent);
-                            }
-                        })
-                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // 处理取消按钮点击事件
-                                // 结束所有关联的 Activity，退出应用
-                                finishAffinity();
-                            }
-                        });
-                // 显示对话框
-                AlertDialog dialog = builder.create();
-                dialog.show();
-            } else {
-                // 权限已被授予
-                Toast.makeText(this, "权限已授予", Toast.LENGTH_SHORT).show();
-                status.setText("权限已授予");
-            }
-
-
-            // prefs.edit().putBoolean(KEY_FIRST_RUN, false).apply();
-        } else {
-            // 不是首次运行，不显示对话框
-            Toast.makeText(this, "欢迎回来", Toast.LENGTH_SHORT).show();
-            status.setText("欢迎回来");
-        }
-
         // 设置按钮点击事件
-        button.setOnClickListener(v -> checkPermissions());
+        button.setOnClickListener(v -> main());
+        button.setOnLongClickListener(v -> {
+            // 处理长按事件的逻辑
+            nahida();
+            return true; // 返回true表示事件已处理，不再继续传递
+        });
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.R)
-    private void checkPermissions() {
+    private void main() {
         TextView status = findViewById(R.id.status);
-        if (!Environment.isExternalStorageManager()){
-            // 请求权限
-            Toast.makeText(this, "未授权，正在拉起设置", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
-            intent.setData(Uri.parse("package:" + getPackageName()));
-            startActivity(intent);
+        if (!requestPermission()) {
+            // 申请权限
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
         } else {
-            // 权限已被授予，执行相关操作
-            Toast.makeText(this, "权限已授权", Toast.LENGTH_SHORT).show();
-            status.setText("权限已授权");
-            // performStorageOperation();
+            status.setText("已有存储权限，尝试写入1.txt测试文本到Andro/data目录");
+            write_text_to_data_directory();
         }
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.R)
+
+    @SuppressLint("NewApi")
+    private boolean requestPermission() {
+        TextView status = findViewById(R.id.status);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Build.VERSION.SDK_INT <= Build.VERSION_CODES.Q) {
+            // 当前设备的Android版本在Android 6.0（API 23）到Android 10（API 29）之间
+            // 先判断有没有权限
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                // 有权限，返回True
+                Toast.makeText(this, "已获得存储权限", Toast.LENGTH_SHORT).show();
+                return true;
+            } else {
+                return false;
+//                // 申请权限
+//                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
+            }
+        } else {
+            // 当前设备的Android版本是Android 11（API 30）及以上
+            if (!Environment.isExternalStorageManager()){
+                // 请求权限
+                Toast.makeText(this, "未授权，正在拉起设置", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                intent.setData(Uri.parse("package:" + getPackageName()));
+                startActivity(intent);
+            } else {
+                // 权限已被授予，执行相关操作
+                Toast.makeText(this, "已授权所有文件管理权限", Toast.LENGTH_SHORT).show();
+                status.setText("已授权所有文件管理权限");
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        TextView status = findViewById(R.id.status);
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (!Environment.isExternalStorageManager()) {
-            // 应用没有MANAGE_EXTERNAL_STORAGE权限
-            status.setText("未获得MANAGE_EXTERNAL_STORAGE权限");
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("提示")
-                    .setMessage("未获得MANAGE_EXTERNAL_STORAGE权限\n是否前往设置授予？")
-                    .setPositiveButton("是", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            // 处理确定按钮点击事件
-                            Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
-                            intent.setData(Uri.parse("package:" + getPackageName()));
-                            startActivity(intent);
-                        }
-                    })
-                    .setNegativeButton("否", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            // 处理取消按钮点击事件
-                            Toast.makeText(MainActivity.this, "已取消授权", Toast.LENGTH_SHORT).show();
-                            status.setText("已取消授权");
-                        }
-                    });
-            // 显示对话框
-            AlertDialog dialog = builder.create();
-            dialog.show();
-        } else {
-            // 应用拥有MANAGE_EXTERNAL_STORAGE权限
-            status.setText("已获得MANAGE_EXTERNAL_STORAGE权限");
+        if (requestCode == REQUEST_CODE) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "存储权限获取成功", Toast.LENGTH_SHORT).show();
+                //在此填写文件操作代码
+            } else {
+                Toast.makeText(this, "存储权限获取失败", Toast.LENGTH_SHORT).show();
+            }
         }
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    private void manage_storage() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // 检查MANAGE_EXTERNAL_STORAGE权限
+            if (!Environment.isExternalStorageManager()) {
+                // 请求MANAGE_EXTERNAL_STORAGE权限
+                Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                intent.setData(Uri.parse("package:" + getPackageName()));
+                startActivity(intent);
+            }
+        }
+    }
+
+    //检查文件能否写入到data目录，如果不能，就拉起document-ui来授权QQ数据目录
+    private void write_text_to_data_directory() {
+        TextView status = findViewById(R.id.status);
+        String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Android/data/1.txt";
+        File file = new File(filePath);
+        try (FileOutputStream fos = new FileOutputStream(file);
+             OutputStreamWriter osw = new OutputStreamWriter(fos, "UTF-8")) {
+            osw.write("纳西妲世界第一可爱！\n\n香草味的纳西妲");
+            Toast.makeText(this, "文件写入成功", Toast.LENGTH_SHORT).show();
+            status.setText("写入成功！在/sdcard/Android/data/1.txt中");
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "文件写入失败", Toast.LENGTH_SHORT).show();
+            status.setText("文件写入失败！");
+        }
+    }
+
+
 
     private void performStorageOperation() {
         // 执行存储操作
